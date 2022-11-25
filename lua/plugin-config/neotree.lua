bd17d2fc94fd412e28e3044226418a9bd2619b75
FZZkill <changzhijie11@outlook.com>
2022-11-25 21:09:47 +0800

    remove some default config

--- lua/plugin-config/neotree.lua
+++ lua/plugin-config/neotree.lua
@@ -72,7 +72,7 @@ require("neo-tree").setup({
     },
 
     window = {
-        position = "right",
+        --position = "right",
         width = 30,
 
         mapping_options = {
@@ -85,48 +85,10 @@ require("neo-tree").setup({
                 "toggle_node",
                 nowait = false -- disable `nowait` if you have existing combos starting with this char that you want to use
             },
-            ["<2-LeftMouse>"] = "open",
-            ["<cr>"] = "open",
-            ["<esc>"] = "revert_preview",
             ["P"] = {"toggle_preview", config = {use_float = true}},
-            ["S"] = "open_split",
-            ["s"] = "open_vsplit",
-            -- ["S"] = "split_with_window_picker",
-            -- ["s"] = "vsplit_with_window_picker",
-            ["t"] = "open_tabnew",
-            -- ["<cr>"] = "open_drop",
-            -- ["t"] = "open_tab_drop",
-            ["w"] = "open_with_window_picker",
-            --["P"] = "toggle_preview", -- enter preview mode, which shows the current node without focusing
-            ["C"] = "close_node",
-            ["z"] = "close_all_nodes",
-            --["Z"] = "expand_all_nodes",
-            ["a"] = {
-                "add",
-                -- some commands may take optional config options, see `:h neo-tree-mappings` for details
-                config = {
-                    show_path = "none" -- "none", "relative", "absolute"
-                }
-            },
-            ["A"] = "add_directory", -- also accepts the optional config.show_path option like "add".
-            ["d"] = "delete",
-            ["r"] = "rename",
-            ["y"] = "copy_to_clipboard",
-            ["x"] = "cut_to_clipboard",
-            ["p"] = "paste_from_clipboard",
-            ["c"] = "copy", -- takes text input for destination, also accepts the optional config.show_path option like "add":
-            -- ["c"] = {
-            --  "copy",
-            --  config = {
-            --    show_path = "none" -- "none", "relative", "absolute"
-            --  }
-            --}
-            ["m"] = "move", -- takes text input for destination, also accepts the optional config.show_path option like "add".
             ["q"] = "close_window",
             ["R"] = "refresh",
-            ["?"] = "show_help",
-            ["<"] = "prev_source",
-            [">"] = "next_source"
+            ["<F1>"] = "show_help",
         }
     },
 
@@ -164,34 +126,6 @@ require("neo-tree").setup({
                 ["<c-x>"] = "clear_filter",
                 ["g["] = "prev_git_modified",
                 ["g]"] = "next_git_modified"
-            }
-        }
-    },
-    buffers = {
-        follow_current_file = true, -- This will find and focus the file in the active buffer every
-        -- time the current file is changed while the tree is open.
-        group_empty_dirs = true, -- when true, empty folders will be grouped together
-        show_unloaded = true,
-        window = {
-            mappings = {
-                ["bd"] = "buffer_delete",
-                ["<bs>"] = "navigate_up",
-                ["."] = "set_root"
-            }
-        }
-    },
-    git_status = {
-        window = {
-            position = "float",
-            mappings = {
-                ["gA"] = "git_add_all",
-                ["gu"] = "git_unstage_file",
-                ["ga"] = "git_add_file",
-                ["gr"] = "git_revert_file",
-                ["gc"] = "git_commit",
-                ["gP"] = "git_push",
-                ["gp"] = "git_pull",
-                ["gg"] = "git_commit_and_push"
             }
         }
     }
