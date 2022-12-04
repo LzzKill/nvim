f8e89dd9430b06e4c26b294cd8ec7025bd4cc9a5
FZZkill <changzhijie11@outlook.com>
2022-12-04 11:01:04 +0800

    remove vim.g and vim.cmd

--- lua/cfg.lua
+++ lua/cfg.lua
@@ -14,28 +14,6 @@ vim.opt.ww = "<,>,h,l"
 vim.opt.wildmenu = true
 vim.opt.wrap = true
 vim.opt.wrapmargin = 2
-vim.g["table_mode_corner"] = "|"
-vim.g["table_mode_border"] = 0
-vim.g["table_mode_fillchar"] = "="
-
-vim.cmd[[
-function! s:isAtStartOfLine(mapping)
-    let text_before_cursor = getline('.')[0 : col('.')-1]
-    let mapping_pattern = '\V' . escape(a:mapping, '\')
-    let comment_pattern = '\V' . escape(substitute(&l:commentstring, '%s.*$', '', ''), '\')
-    return (text_before_cursor =~? '^' . ('\v(' . comment_pattern . '\v)?') . '\s*\v' . mapping_pattern . '\v$')
-endfunction
-au Filetype FILETYPE let b:AutoPairs = {"(": ")"}
-inoreabbrev <expr> <bar><bar>
-\ <SID>isAtStartOfLine('\|\|') ?
-\ '<c-o>:TableModeEnable<cr><bar><space><bar><left><left>' : '<bar><bar>'
-inoreabbrev <expr> __
-\ <SID>isAtStartOfLine('__') ?
-\ '<c-o>:silent! TableModeDisable<cr>' : '__'
-"set fillchars=eob:\ ,vert:\ 
-set fillchars=vert:\ 
-]]
-
 -- if neoclide
 vim.cmd[[
 if exists("g:neovide")
