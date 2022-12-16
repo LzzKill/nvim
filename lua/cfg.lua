e1a662cf5b04a28840dd736e3f099c1ca1a549fe
FZZkill <changzhijie11@outlook.com>
2022-12-16 20:57:27 +0800

    choose all tab is 2

--- lua/cfg.lua
+++ lua/cfg.lua
@@ -5,20 +5,15 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.hls = true
 vim.opt.ruler = true
-vim.opt.shiftwidth = 4
+vim.opt.shiftwidth = 2
 vim.opt.scrolloff = 5
-vim.opt.sidescrolloff = 4
+vim.opt.sidescrolloff = 2
 vim.opt.textwidth = 80
-vim.opt.tabstop = 4
+vim.opt.tabstop = 2
 vim.opt.ww = "<,>,h,l"
 vim.opt.wildmenu = true
 vim.opt.wrap = true
 vim.opt.wrapmargin = 2
--- if neoclide
-vim.cmd[[
-if exists("g:neovide")
-    let neovide_cursor_vfx_mode = "railgun"
-    let neovide_scale_factor = 0.8
-    let neovide_transparency = 0.8
-endif
-]]
+
+
+
