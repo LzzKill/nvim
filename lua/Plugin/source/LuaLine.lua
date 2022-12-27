d1efa1449a80b06dff19221aaff962cea5df2bcb
FZZkill <changzhijie11@outlook.com>
2022-12-27 21:40:34 +0800

    [fix] LuaLine plug

--- lua/Plugin/source/LuaLine.lua
+++ lua/Plugin/source/LuaLine.lua
@@ -1,25 +1,27 @@
 local noice = require "noice"
-require"lualine".setup{{
+require("lualine").setup({
   options = {
     icons_enabled = true,
     theme = "auto",
     component_separators = "|",
-    section_separators = {left = "", right = ""}
   },
   sections = {
-    lualine_a = {{"mode", separator = {left = ""}, right_padding = 2}},
-    lualine_b = {"filename", "branch"},
-    lualine_c = {"fileformat"},
+    lualine_b = { "filename", "branch" },
+    lualine_c = { "fileformat" },
     lualine_x = {
       {
-        require("noice").api.status.command.get,
+        noice.api.status.command.get,
         cond = require("noice").api.status.command.has,
         color = { fg = "#ff9e64" },
       },
+      {
+        noice.api.status.mode.get,
+        cond = require("noice").api.status.mode.has,
+        color = { fg = "#ff9e64" },
+      },
     },
-    lualine_y = {"filetype", "progress"},
+    lualine_y = { "filetype", "progress" },
     lualine_z = {
-      {"location", separator = {right = ""}, left_padding = 2}
     }
   }
-}}
+})
