635807e1dab32b5a132e253e751cb3680ceaa4fc
FZZkill <changzhijie11@outlook.com>
2023-01-15 20:47:00 +0800

    remove too

--- init.lua
+++ init.lua
@@ -1,18 +1,3 @@
--- GetLazy
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "--single-branch",
-    "https://github.com/folke/lazy.nvim.git",
-    lazypath,
-  })
-end
--- Leader Other
+require("Plugin")
 vim.opt.listchars:append"space:⋅"
 vim.api.nvim_set_keymap("n", ";", ":", {})
--- Leader Plugin and Configs
-vim.opt.runtimepath:prepend(lazypath)
-require("Plugin")
