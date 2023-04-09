9eb18f9f4f827c90334c6853968fa8437d1d66fe
FZZkill <changzhijie11@outlook.com>
2023-04-09 19:53:21 +0800

    [fix] 这是vim的键位设置

---
+++ plugin/Keymap.lua
@@ -0,0 +1,24 @@
+local Map = {
+  { ";",         ":",                      "n", {} },
+  { "U",         "<C-r>" },
+  { "<A-[>",     vim.diagnostic.goto_prev },
+  { "<A-]>",     vim.diagnostic.goto_next },
+  { "<leader>L", "<cmd>Lazy<cr>" },
+  { "<leader>t", "<cmd>Telescope<cr>" },
+  { "<leader>q", vim.diagnostic.setloclist },
+  { "<space>w",  "<cmd>w<cr>" },
+  { "<space>q",  "<cmd>q<cr>" },
+  { "<space>Q",  "<cmd>q!<cr>" },
+  { "<space>f",  "<cmd>Telescope fd<cr>" },
+  { "<space>e",  "<cmd>NvimTreeToggle<cr>" },
+  { "<space>E",  vim.diagnostic.open_float },
+}
+local opt = { noremap = true, silent = true }
+
+for _, m in ipairs(Map) do
+  local n = "n"
+  local o = opt
+  if m[3] then n = m[3] end
+  if m[4] then o = m[4] end
+  vim.keymap.set(n, m[1], m[2], o)
+end
