ea353a07a27a5b2d740063a68a35b2c1816eb4a9
FZZkill <changzhijie11@outlook.com>
2023-05-13 14:15:57 +0800

    [fix] 结构更新

--- init.lua
+++ init.lua
@@ -1,22 +1,7 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-vim.opt.runtimepath:prepend(lazypath)
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "--single-branch",
-    "https://github.com/folke/lazy.nvim.git",
-    "--depth 1",
-    lazypath,
-  })
-end
-local plugins = require("plugins")
-require("lazy").setup(plugins, {
-  ui = {
-    border = "rounded",
-  },
-}
-)
+local uv = vim.loop
 
-require("modules")
+require("basic.Keymap")
+require("basic.Setting")
+require("basic.Lazy")
+
+require("modules.welcome")
