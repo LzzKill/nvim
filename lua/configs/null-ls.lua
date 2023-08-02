a2b95dad0712d99218ef1980d43ccffeace42dcb
FZZkill <changzhijie11@outlook.com>
2023-08-02 22:08:05 +0800

    [fix]

--- lua/configs/null-ls.lua
+++ lua/configs/null-ls.lua
@@ -1,9 +1,9 @@
 return function()
   local null_ls = require("null-ls")
   null_ls.setup({
-    -- on_init = function(new_client, _)
-    --   new_client.offset_encoding = 'utf-8'
-    -- end,
+    on_init = function(new_client, _)
+      new_client.offset_encoding = 'utf-8'
+    end,
     sources = {
       null_ls.builtins.formatting.black,
       null_ls.builtins.formatting.prettier,
