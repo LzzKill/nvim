c056dc0227e435f88362238cc5193caddb611aa3
FZZkill <changzhijie11@outlook.com>
2023-08-03 17:34:35 +0800

    [add] 添加了toggleterm配置

---
+++ lua/configs/toggleterm.lua
@@ -0,0 +1,24 @@
+require("toggleterm").setup({
+  -- size can be a number or function which is passed the current terminal
+  size = 20,
+  open_mapping = "<space>t",
+  direction = "float",
+  close_on_exit = true, -- close the terminal window when the process exits
+   -- Change the default shell. Can be a string or a function returning a string
+  shell = vim.o.shell,
+  auto_scroll = true, -- automatically scroll to the bottom on terminal output
+  -- This field is only relevant if direction is set to 'float'
+  float_opts = {
+    -- The border key is *almost* the same as 'nvim_open_win'
+    -- see :h nvim_open_win for details on borders however
+    -- the 'curved' border is a custom border type
+    -- not natively supported but implemented in this plugin.
+    border = "double"
+  },
+  winbar = {
+    enabled = true,
+    name_formatter = function(term) --  term: Terminal
+      return term.name
+    end
+  },
+})
